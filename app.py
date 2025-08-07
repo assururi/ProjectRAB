cd7f5f056169cba540431bd6f38af2ad842e8db2
Naufal As-Sururi <106474889+assururi@users.noreply.github.com>
2025-08-07 09:01:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ st.markdown(
     .stForm > div {
         border: 2px solid #00468C !important; /* Biru PLN */
         border-radius: 8px;
-        background-color: #FFD700; /* Contoh: kuning solid */
+        background-color: #00000; /* Contoh: kuning solid */
         padding: 20px;
     }
     </style>
